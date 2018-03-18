e79299e2b2e657fd115dbf2a7db4826b0997223f
evan-jaramillo <37516927+evan-jaramillo@users.noreply.github.com>
2018-03-18 15:33:28 -0500

    Add files via upload

---
+++ greek.h
@@ -0,0 +1,59 @@
+/*
+    Definitions for greek characters
+    By: Evan Jaramillo 2018
+*/
+class greek
+{
+    //UPPER CASE GREEK
+
+    #define     G_ALPHA   "\u0391" //A
+    #define     G_BETA    "\u0392" //B
+    #define     G_GAMMA   "\u0393" //Γ
+    #define     G_DELTA   "\u0394" //Δ
+    #define     G_EPSILON "\u0395" //Ε
+    #define     G_ZETA    "\u0396" //Ζ
+    #define     G_ETA     "\u0397" //Η
+    #define     G_THETA   "\u0398" //Θ
+    #define     G_IOTA    "\u0399" //Ι
+    #define     G_KAPPA   "\u039A" //Κ
+    #define     G_LAMBDA  "\u039B" //Λ
+    #define     G_MU      "\u039C" //Μ
+    #define     G_NU      "\u039D" //Ν
+    #define     G_XI      "\u039E" //Ξ
+    #define     G_OMICRON "\u039F" //Ο
+    #define     G_PI      "\u03A0" //Π
+    #define     G_RHO     "\u03A1" //Ρ
+    #define     G_SIGMA   "\u03A3" //Σ
+    #define     G_TAU     "\u03A4" //Τ
+    #define     G_UPSILON "\u03A5" //Υ
+    #define     G_PHI     "\u03A6" //Φ
+    #define     G_CHI     "\u03A7" //Χ
+    #define     G_PSI     "\u03A8" //Ψ
+    #define     G_OMEGA   "\u03A9" //Ω
+
+    //Lower Case Greek
+    #define     G_alpha   "\u03B1" //α
+    #define     G_beta    "\u03B2" //β
+    #define     G_gamma   "\u03B3" //γ
+    #define     G_delta   "\u03B4" //δ
+    #define     G_epsilon "\u03B5" //ε
+    #define     G_zeta    "\u03B6" //ζ
+    #define     G_eta     "\u03B7" //η
+    #define     G_theta   "\u03B8" //θ
+    #define     G_iota    "\u03B9" //ι
+    #define     G_kappa   "\u03BA" //κ
+    #define     G_lambda  "\u03BB" //λ
+    #define     G_mu      "\u03BC" //μ
+    #define     G_nu      "\u03BD" //ν
+    #define     G_xi      "\u03BE" //ξ
+    #define     G_omicron "\u03BF" //ο
+    #define     G_pi      "\u03C0" //π
+    #define     G_rho     "\u03C1" //ρ
+    #define     G_sigma   "\u03C3" //σ
+    #define     G_tau     "\u03C4" //τ
+    #define     G_upsilon "\u03C5" //υ
+    #define     G_phi     "\u03C6" //φ
+    #define     G_chi     "\u03C7" //χ
+    #define     G_psi     "\u03C8" //ψ
+    #define     G_omega   "\u03C9" //ω
+};
